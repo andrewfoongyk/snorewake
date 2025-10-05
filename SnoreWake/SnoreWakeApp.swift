f420f45e9e4f3837bad45f0575f9176dd36111ce
Andrew Foong <ykf21@cam.ac.uk>
2025-10-05 13:40:54 -0500

    Initial Commit

---
+++ SnoreWake/SnoreWakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoreWakeApp.swift
+//  SnoreWake
+//
+//  Created by Andrew Foong on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnoreWakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
